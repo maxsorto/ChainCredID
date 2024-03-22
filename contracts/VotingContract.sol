c57693da30817c6dcfcac39476cd7518f74b0df4
jramirez09 <jramirez12@luc.edu>
2024-03-22 16:36:44 -0600

    fixed import

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
 contract VotingContract is VRFConsumerBase {
     uint256 public proposalCount;
